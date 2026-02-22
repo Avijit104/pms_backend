0e50cc5fd7c5e7332e081d3bb0233bb1080cde48
Avijit Ash <avijitash06@gmail.com>
2026-02-22 23:07:54 +0530

    added constant file

---
+++ src/util/constants.js
@@ -0,0 +1,12 @@
+export const roles = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member",
+}
+export const taskStatus = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done",
+}
+export const availableRoles = Object.values(roles)
+export const avaliableTaskStatus = Object.values(taskStatus)
