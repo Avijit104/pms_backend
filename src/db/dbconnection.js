4cd151cb96b378432d6445eec2a947b0c7bfa96a
Avijit Ash <avijitash06@gmail.com>
2026-02-23 11:16:40 +0530

    added database connection

---
+++ src/db/dbconnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        console.log("db connection successful")
+    } catch (error) {
+        console.error("an error occured in db connection", error)
+        process.exit(1)
+    }
+}
